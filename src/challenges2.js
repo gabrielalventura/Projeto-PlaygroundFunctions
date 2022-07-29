6f1fa54ccd50b1cad424c983fd1be730f1fe46b5
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-07-29 10:54:53 -0300

    Finalização do requisito 11. Bonus. 100 de projeto. VQV

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,21 @@
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(celNumber) {
+  if (celNumber.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  let quantit = 0;
+  for (let number of celNumber) {
+    for (let index = 0; index < celNumber.length; index += 1) {
+      if (number === celNumber[index]) {
+        quantit += 1;
+      }
+      if (quantit >= 3 || number < 0 || number > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    } 
+    quantit = 0;
+    }
+  return ('(' + celNumber[0] + celNumber[1] + ') ' + (celNumber.slice(2, 7)).join('') + '-' + (celNumber.slice(7, 11)).join(''));
 }
 
 // Desafio 12
@@ -16,10 +32,10 @@ function triangleCheck(lineA, lineB, lineC) {
 function hydrate(drinks) {
   let quant = drinks.replace(/\D+/g, '');
   let water = 0;
-  for (let index = 0; index < quant.length; index +=1){
+  for (let index = 0; index < quant.length; index += 1) {
     water += Number(quant[index]);
   }
-  if(water === 1){
+  if (water === 1) {
     return water + ' copo de água';
   } return water + ' copos de água';
 }
